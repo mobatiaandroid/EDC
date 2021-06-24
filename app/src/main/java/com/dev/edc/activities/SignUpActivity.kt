5243593dbabcd32a8055e1d3298b2d372a9ce36b
mobatiaandroid <sanju.sabu@mobatia.com>
2021-06-24 17:41:22 +0530

    Sign Up

--- app/src/main/java/com/dev/edc/activities/SignUpActivity.kt
+++ app/src/main/java/com/dev/edc/activities/SignUpActivity.kt
@@ -1,11 +1,13 @@
 package com.dev.edc.activities
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import com.dev.edc.R
 
 class SignUpActivity : AppCompatActivity() {
@@ -22,6 +24,24 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     private fun initialiseUI() {
-        TODO("Not yet implemented")
+        backButton = findViewById(R.id.backButton)
+        trafficNumber = findViewById(R.id.trafficNumber)
+        tryFileNo = findViewById(R.id.tryFileNo)
+        proceedButton = findViewById(R.id.proceedButton)
+        backButton.setOnClickListener {
+            val intent = Intent(context, AccountActivity::class.java)
+            startActivity(intent)
+        }
+        proceedButton.setOnClickListener {
+            if(trafficNumber.text.toString().trim().equals("", ignoreCase = true)) {
+                Toast.makeText(this, "Field cannot be empty", Toast.LENGTH_SHORT).show();
+            } else if(tryFileNo.text.toString().trim().equals("", ignoreCase = true)) {
+            Toast.makeText(this, "Field cannot be empty", Toast.LENGTH_SHORT).show();
+            } else {
+                val intent = Intent(context, SignUpDetailActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/dev/edc/activities/SignUpDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.dev.edc.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dev.edc.R
+
+class SignUpDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_detail)
+    }
+}
