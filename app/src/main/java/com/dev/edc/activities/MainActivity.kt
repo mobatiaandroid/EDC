152196659f77ca959b1adc9cf187235982b3ed67
mobatiaandroid <sanju.sabu@mobatia.com>
2021-06-24 14:57:25 +0530

    Initial Commit

---
+++ app/src/main/java/com/dev/edc/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+
+package com.dev.edc.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dev.edc.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
